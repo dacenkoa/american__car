2464e439fdea9c9a78d0f658ced27fbe736e6491
dacenkoa <125560792+dacenkoa@users.noreply.github.com>
2024-03-12 17:52:57 +0100

    made sections: information, slider.

---
+++ app/js/main.js
@@ -0,0 +1,9 @@
+$('.slick__slider').slick({
+    dots: true,
+    arrows: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    // focusOnSelect: true,
+    centerMode: true, 
+    centerPadding: "50px",
+});
