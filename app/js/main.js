fd7d8784b3272125c0f88720e6d2e2b7ce5684d1
dacenkoa <125560792+dacenkoa@users.noreply.github.com>
2024-03-26 13:29:49 +0100

    uphill button made

--- app/js/main.js
+++ app/js/main.js
@@ -28,3 +28,17 @@ $('.burger__menu').on('click', function () {
     $('.burger__menu').toggleClass('show');
     $('.header__content').toggleClass('show');
 });
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('.scrollToTopBtn').addClass('showBtn');
+        } else {
+            $('.scrollToTopBtn').removeClass('showBtn');
+        }
+    });
+    
+    $('.scrollToTopBtn').click(function(){
+        $('html, body').animate({scrollTop : 0},800);
+        return false;
+    });
+});
